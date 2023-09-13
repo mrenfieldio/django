711c340df137a35de923a66df6ec2f653afbff2b
ashish <ashishkshaji2002@gmail.com>
2023-09-13 15:56:47 +0530

    hai

---
+++ amazon/product/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class pro(models.Model):
+    name=models.CharField(max_length=20)
+    price=models.IntegerField() 
+
+    def __str__(self):
+        return self.name
+    
+class user(models.Model):
+    name=models.CharField(max_length=15)
+    email=models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
